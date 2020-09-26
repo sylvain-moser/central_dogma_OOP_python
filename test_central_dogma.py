f2edded6c476cdb26a50d98c9f762d54569fdcc8
sylvain <sylvain_moser@psych.mpg.de>
2020-09-26 19:27:15 +0200

    copied test from mob prog and corrected

--- test_central_dogma.py
+++ test_central_dogma.py
@@ -1,4 +1,20 @@
 from central_dogma import *
 
-def test_add():
-    assert add(1,1)==2
+
+def test_antisense():
+    assert antisense("TTAGGGCATG") == "CATGCCCTAA"
+
+
+def test_transcribe_to_rna():
+    assert transcribe_to_rna("TTAGGGCATG") == 'CAUGCCCUAA'
+
+
+def test_codon_to_prot():
+    assert codon_to_prot("AUG") == "Met"
+
+def test_prot3_to_prot1():
+    assert prot3_to_prot1("Met")=="M"
+
+
+def test_translation():
+    assert translation('CAUGCCCUAA') == ("MetPro",'MP')
